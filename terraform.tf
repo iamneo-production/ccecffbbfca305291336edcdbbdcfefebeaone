571d1751a5e069de935b42725924d0b8135f32a2
Iamneo <techadmin@iamneo.ai>
2023-09-21 13:00:31 +0000

    Backup Commit

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "ap.northeast-1"
+    region = "ap-northeast-1"
     access_key = "AKIAY6OXM4NMALTE27NW"
     secret_key = "5eGUQxiqXM9Zt/MMSBji5A13hUrbGoomy0ZCINIr"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "linux_server" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
 
@@ -12,5 +12,5 @@ resource "aws_instance" "example" {
     }
 }
 output "public_ip" {
-    value = aws_instance.example.public_ip
+    value = aws_instance.linux_server.public_ip
 }
